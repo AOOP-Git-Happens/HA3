b9cab067a88d1fe427c3e41b7342fa032b5cb557
Marlo Broch <jbroc24@student.sdu.dk>
2026-04-15 03:04:53 +0200

    ui setup view 3
    - added button for exporting - no function atm
    - made the 3 headers
    - added a datepicker for busiest airport and most flights.
    - added the json in assest

--- FlightTracker/ViewModels/AnalyticsViewModel.cs
+++ FlightTracker/ViewModels/AnalyticsViewModel.cs
@@ -1,13 +1,21 @@
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
 namespace FlightTracker.ViewModels;
 
 /// <summary>
 /// ViewModel 3. LINQ analytics, charts, export.
 /// </summary>
-
 public partial class AnalyticsViewModel : ViewModelBase
 {
-     public AnalyticsViewModel()
+    public AnalyticsViewModel()
     {
-        Header = "Analytics"; //tab name
+        Header = "Analytics"; // tab name
+    }
+
+    [RelayCommand]
+    private void Export()
+    {
+        // To be implemented later
     }
 }
