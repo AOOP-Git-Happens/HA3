1080bf360bb865cc3fac2263c8987e81f5f4291f
alikrist <144455345+alikrist@users.noreply.github.com>
2026-04-17 14:12:18 +0200

    Added widgets, such as zoomINOut, coordinates, added alignment

--- FlightTracker/Views/RouteMapView.axaml.cs
+++ FlightTracker/Views/RouteMapView.axaml.cs
@@ -1,5 +1,12 @@
 using Avalonia.Controls;
 using Mapsui.Tiling;
+using Mapsui.Extensions;
+using Mapsui.Widgets;
+using Mapsui.Widgets.ScaleBar;
+using Mapsui.Widgets.ButtonWidgets;
+using System.Threading.Tasks;
+using Mapsui.Widgets.InfoWidgets;
+using Mapsui;
 
 namespace FlightTracker.Views;
 
@@ -8,6 +15,29 @@ public partial class RouteMapView : UserControl
     public RouteMapView()
     {
         InitializeComponent();
-        MyMapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
+        MyMapControl.Map = CreateMap();
+    }
+
+    private static Map CreateMap()
+    {
+        Map map = new Map
+        {
+            //CRS is Coordinate Reference System, projects earth in flat surface that map tiles, markers, routes
+            CRS = "EPSG:3857" 
+        };
+
+        map.Layers.Add(OpenStreetMap.CreateTileLayer()); //map is not one big image, it split into small square images
+        map.Widgets.Add(new ScaleBarWidget(map) 
+        {
+            TextAlignment = Alignment.Center,
+            HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Center,
+            VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Top
+        });
+        map.Widgets.Add(new ZoomInOutWidget
+        {
+            Margin = new MRect(20, 50) //widget position
+        });
+        map.Widgets.Add(new MouseCoordinatesWidget());
+        return map;
     }
 }
