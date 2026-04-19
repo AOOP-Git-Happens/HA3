7c625bfbb1a810b708770aa3268f834c05964f76
Marlo Broch <jbroc24@student.sdu.dk>
2026-04-19 15:50:08 +0200

    test

--- FlightTracker/ViewModels/AnalyticsViewModel.cs
+++ FlightTracker/ViewModels/AnalyticsViewModel.cs
@@ -2,6 +2,11 @@ using CommunityToolkit.Mvvm.Input;
 using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Linq;
+using System.Collections.Generic;
+using FlightTracker.Models;
 
 namespace FlightTracker.ViewModels;
 
@@ -11,6 +16,7 @@ namespace FlightTracker.ViewModels;
 
 public partial class AnalyticsViewModel : ViewModelBase
 {
+    
     public AnalyticsViewModel()
     {
         Header = "Analytics"; // tab name
@@ -24,4 +30,14 @@ public partial class AnalyticsViewModel : ViewModelBase
 
     [ObservableProperty]
     private DateTimeOffset? _SelectedDate;
+
+            public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { 2 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 1 } },
+                new PieSeries<double> { Values = new double[] { 4 } },
+                new PieSeries<double> { Values = new double[] { 3 } }
+            };
 }
