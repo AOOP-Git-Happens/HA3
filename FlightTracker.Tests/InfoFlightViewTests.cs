af65acaedef74b0600f7872c920878f7c452d4af
Kyrylosa <savirskiyk@gmail.com>
2026-04-21 02:50:12 +0200

    Create InfoFlightViewTests.cs

---
+++ FlightTracker.Tests/InfoFlightViewTests.cs
@@ -0,0 +1,28 @@
+using FlightTracker.ViewModels;
+using Avalonia.Headless; // enables UI testing without opening real windows
+using Avalonia.Headless.XUnit; // integrates Avalonia with xUnit
+using xUnit;
+
+namespace FlightTracker.Tests;
+
+// Test cases that cover at least ViewModel logic and LINQ query results
+
+public class InfoFlightViewModelTests
+{
+    public readonly InfoFlightViewModel _infoFlightViewModel;
+    public InfoFlightViewModelTests()
+    {
+        // SUT (System Under Test)
+        _infoFlightViewModel = new InfoFlightViewModel();
+    }
+
+    [Fact]
+    public void InfoFlightViewModel_ClearSelection_SetsNull
+    {
+        //Arrange (var
+
+        //Act
+        var 
+        //Assert
+    }
+}
